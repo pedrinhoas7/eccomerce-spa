59e50918fbb1ab9c8066a537d849439fc8e03789
pedrinhoas7 <pedrinhoas7@gmail.com>
2024-07-25 22:17:07 -0300

    Feat: create basic component

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <CustomerManagement />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CustomerManagement from '../components/CustomerManagement.vue';
+
+export default defineComponent({
+    name: 'Home',
+    components: { CustomerManagement },
+    setup() {
+
+    }
+})
+</script>
+
+<style scoped></style>
